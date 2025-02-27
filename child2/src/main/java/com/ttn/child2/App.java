82ffb32e0cc4f5f97159462879f3a19b9fb0ebbb
Krishna Sharma <krishna.sharma@tothenew.com>
2025-02-27 22:39:07 +0530

    First Commit

---
+++ child2/src/main/java/com/ttn/child2/App.java
@@ -0,0 +1,11 @@
+package com.ttn.child2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from child-2");
+    }
+}
