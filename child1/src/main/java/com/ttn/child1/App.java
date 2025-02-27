f355231bd51d1e06782fa13d3e2e3bdf57090b1f
Krishna Sharma <krishna.sharma@tothenew.com>
2025-02-27 22:58:25 +0530

    Added Comments

--- child1/src/main/java/com/ttn/child1/App.java
+++ child1/src/main/java/com/ttn/child1/App.java
@@ -6,6 +6,8 @@ package com.ttn.child1;
  */
 public class App
 {
+    // Ques-4
+    // Executed using the Jar Plugin
     public static void main( String[] args )
     {
         System.out.println("Hello from Child 1");
